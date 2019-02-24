37e1294f6bcc03e0a247851b56f1ebeac2a9312a
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-02-24 14:36:11 +0100

    Around

--- Spring/Fakultet/src/main/java/com/fakultet/software/spring/Logger.java
+++ Spring/Fakultet/src/main/java/com/fakultet/software/spring/Logger.java
@@ -1,8 +1,10 @@
 package com.fakultet.software.spring;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -19,25 +21,23 @@ public class Logger {
 		
 	}
 	
-	@Before("addPointcut()")
-	public void prepareToAddSubject(JoinPoint jp){
-		Object[] args=jp.getArgs();
+	@Around("addPointcut()")
+	public void prepareToAddSubject(ProceedingJoinPoint pjp) throws Throwable {
+		Object[] args=pjp.getArgs();
 		String s= (String)args[0];
 		System.out.println("Prepare to add a subject..."+s);
+		
+		try {
+			pjp.proceed();
+			System.out.println("Added..."+s);
+			
+		}catch (Exception e){
+			
+			System.out.println("ERROR: NOT ADDED "+s +"\n");
+		}
 	}
 	
-	@AfterReturning("addPointcut()")
-	public void added(JoinPoint jp){
-		Object[] args=jp.getArgs();
-		String s= (String)args[0];
-		System.out.println("Added..."+s);
-	}
-	@AfterThrowing("addPointcut()")
-	public void error(JoinPoint jp){
-		Object[] args=jp.getArgs();
-		String s= (String)args[0];
-		System.out.println("ERROR: NOT ADDED "+s);
-	}
+	
 	
 	
 }
